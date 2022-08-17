062e88218e600374b3b7cd020057f9bac63561ba
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-08-17 18:05:19 +0500

    Creating Variables of all object in MainActivity, defining their Id's and Implementing Click Listener on Buttons

--- app/src/main/java/com/example/sqlitedatabaseinandroid/MainActivity.java
+++ app/src/main/java/com/example/sqlitedatabaseinandroid/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.sqlitedatabaseinandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etCustomerName, etCustomerAge;
+    Switch switchActiveCustomers;
+    Button btnAdd, btnViewAll;
+    ListView listViewCustomerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etCustomerName = findViewById(R.id.idEtCustomerName);
+        etCustomerAge = findViewById(R.id.idEtCustomerAge);
+        switchActiveCustomers = findViewById(R.id.idSwitchActiveCustomers);
+        btnAdd = findViewById(R.id.idBtnAdd);
+        btnViewAll = findViewById(R.id.idBtnViewAll);
+        listViewCustomerList = findViewById(R.id.idListViewCustomerList);
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "add working", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "view working", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
